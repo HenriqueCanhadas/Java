318d2e389be5056507d15f328988f89734818aff
HenriqueCanhadas <pedrosacanhadas@gmail.com>
2023-10-11 09:54:15 -0300

    att

--- TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -6,7 +6,7 @@ public class TiposPrimitivos {
     public static void main(String[] args) {
         float nota = 8.5f;
         System.out.println("A nota e:"+nota);
-        System.out.println("Oi");
-        System.out.println("abc");
+        System.out.printf("A Nota Seria de:%.2f", nota);
+
     } 
 }
